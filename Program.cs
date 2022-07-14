b40392017a421cd892eb4d68c19df93dd65e4843
Carol <dleandrosilva@hotmail.com>
2022-07-14 19:46:24 -0300

    Projeto IMC 2

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Projeto_IMC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Qual seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite dua idade:");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora digite o seu peso:");
+            float peso = (float) Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Agora digite sua altura:");
+            float altura = (float) Convert.ToDouble(Console.ReadLine());
+
+            float resultado = peso / (altura * altura);
+
+            if(resultado < 18.5)
+            {
+                Console.WriteLine("Você está abaixo do peso.");
+            }
+            else if(resultado >= 18 && resultado < 25)
+            {
+                Console.WriteLine("Você está no peso normal");
+            }
+            else if(resultado >= 25 && resultado < 30)
+            {
+                Console.WriteLine("Você está com excesso de peso");
+            }
+              else if(resultado >= 30)
+            {
+                Console.WriteLine("Você está obeso");
+            }
+        }
+    }
+}
